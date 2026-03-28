55f9f20cb2c0fd32b7c0edbf6bf943aff8dc2e39
sionkim0126 <kimsion000@naver.com>
2026-03-28 21:38:35 +0900

    init common

---
+++ src/test/java/com/github/sionkim0126/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sionkim0126.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
