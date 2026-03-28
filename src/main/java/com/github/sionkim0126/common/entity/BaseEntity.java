edae6b8d573d26dba460990421ee49636db8d597
sionkim0126 <kimsion000@naver.com>
2026-03-29 00:28:21 +0900

    feat : commit-3 라이브러리화를 위한 설정 추가 -jpa의존성 추가 및 baseEntity작성

---
+++ src/main/java/com/github/sionkim0126/common/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.github.sionkim0126.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private UUID createdBy;
+}
