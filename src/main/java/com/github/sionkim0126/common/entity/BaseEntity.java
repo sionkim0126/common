db5837727bd7b13d6b71420b54828651c23233b9
sionkim0126 <kimsion000@naver.com>
2026-03-29 22:46:34 +0900

    update : BaseEntity

--- src/main/java/com/github/sionkim0126/common/entity/BaseEntity.java
+++ src/main/java/com/github/sionkim0126/common/entity/BaseEntity.java
@@ -6,6 +6,8 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -23,4 +25,28 @@ public abstract class BaseEntity {
     @CreatedBy
     @Column(name = "created_by", updatable = false)
     private UUID createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private UUID updatedBy;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by")
+    private UUID deletedBy;
+
+    public void softDelete(UUID userId) {
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = userId;
+    }
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
+
 }
